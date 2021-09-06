a5dbca20f158260dc8b268fd7f648bf962466cf0
Maxim Talanov <mail@maxtalanov.ru>
2021-09-07 02:05:45 +0300

    При регистрации поле avatar не должно быть обязательным.

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -68,7 +68,7 @@ router.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().required().custom(method),
+    avatar: Joi.string().custom(method),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
