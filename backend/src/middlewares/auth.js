8c32e7a607c817610811a4cf1030c145f89b775f
Maxim Talanov <mail@maxtalanov.ru>
2021-09-06 04:05:26 +0300

    Если токен неверный, то должна быть ошибка 401,а не 403.

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -3,14 +3,14 @@ const jwt = require('jsonwebtoken');
 const { NODE_ENV, JWT_SECRET, JWT_DEV = 'some-secret-key' } = process.env;
 const secretKey = NODE_ENV === 'production' ? JWT_SECRET : JWT_DEV;
 
-const ForbiddenErrors = require('../errors/forbidden-err');
+const UnauthorizedErrors = require('../errors/unauthorized-err');
 
 module.exports = (req, res, next) => {
   // const { authorization } = req.headers;
   const cookiesJWT = req.cookies.jwt;
 
   if (!cookiesJWT) {
-    throw new ForbiddenErrors('Необходима авторизация');
+    throw new UnauthorizedErrors('Необходима авторизация');
   }
 
   const token = cookiesJWT.replace('Bearer ', '');
@@ -19,7 +19,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, secretKey);
   } catch (err) {
-    throw new ForbiddenErrors('Необходима авторизация');
+    throw new UnauthorizedErrors('Необходима авторизация');
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса;
