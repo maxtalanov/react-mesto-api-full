97ddec12a2036b8064f541e2a507a7fdabb117f3
Maxim Talanov <mail@maxtalanov.ru>
2021-09-06 04:30:36 +0300

    Исправить метод проверки is URL для валидотора и кастомной ф-ии JOI Celebrete

--- backend/src/routes/cards.js
+++ backend/src/routes/cards.js
@@ -1,10 +1,19 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
+const validator = require('validator');
 const auth = require('../middlewares/auth');
-const { regUrl } = require('../utils/const');
 const {
   createCard, getCards, deleteCard, addLikesCard, deleteLikesCard,
 } = require('../controllers/card');
+
+// 0.1: метод проверки URL на валидность;
+const method = (value) => {
+  const result = validator.isURL(value, { require_protocol: true });
+  if (result) {
+    return value;
+  }
+  throw new Error('URL в веден не корректно');
+};
 
 // 1. Возвращает все карточки
 router.get('/cards', auth, getCards);
@@ -13,7 +22,7 @@ router.get('/cards', auth, getCards);
 router.post('/cards', auth, celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().pattern(regUrl),
+    link: Joi.string().required().custom(method),
   }),
 }), createCard);
 
--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,11 +1,20 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
+const validator = require('validator');
 const auth = require('../middlewares/auth');
-const { regUrl } = require('../utils/const');
 
 const {
   getUser, getUsers, createUser, upDateUser, upDataUserAvatar, login, getUserMe, exit,
 } = require('../controllers/users');
+
+// 0.1: метод проверки URL на валидность;
+const method = (value) => {
+  const result = validator.isURL(value, { require_protocol: true });
+  if (result) {
+    return value;
+  }
+  throw new Error('URL в веден не корректно');
+};
 
 // 1. Получить мои данные
 router.get('/users/me', auth, getUserMe);
@@ -25,7 +34,7 @@ router.post('/users', auth, celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(regUrl),
+    avatar: Joi.string().custom(method),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
@@ -42,7 +51,7 @@ router.patch('/users/me', auth, celebrate({
 // 6. Обновляет аватар
 router.patch('/users/me/avatar', auth, celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().required().pattern(regUrl),
+    avatar: Joi.string().required().custom(method),
   }),
 }), upDataUserAvatar);
 
@@ -59,7 +68,7 @@ router.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(regUrl),
+    avatar: Joi.string().required().custom(method),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
