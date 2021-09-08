dfc33b85ef2e9bbbbbbf9405875af5d835f7a57c
Maxim Talanov <mail@maxtalanov.ru>
2021-09-08 16:46:10 +0300

    Добавить новые параметры для куки

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -134,8 +134,8 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          // sameSite: 'None',
-          // secure: true,
+          sameSite: 'None',
+          secure: true,
         })
         .send({ message: 'Авторизация успешно пройдена' });
     })
